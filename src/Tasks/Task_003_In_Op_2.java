c504cf8f3d2e709f18fced9b9bf9c33556d718f6
bhaktivazare8 <bhaktivazare8@gmail.com>
2025-06-30 22:06:16 +0530

    30th June

---
+++ src/Tasks/Task_003_In_Op_2.java
@@ -0,0 +1,10 @@
+package Tasks;
+
+public class Task_003_In_Op_2 {
+
+        public static void main(String[] args) {
+            int a = 20;
+            System.out.println(--a + a++ + a--);
+        }
+    }
+
